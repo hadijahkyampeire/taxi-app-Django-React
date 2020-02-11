faab6c55c24612417a430db95e6c0cfe0cc8dc01
hadijahkyampeire <hadijah315@gmail.com>
2020-02-11 20:47:09 +0300

    Use the websocket and django channels to create trips and update them

--- server/taxi/taxi/routing.py
+++ server/taxi/taxi/routing.py
@@ -1,3 +1,13 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
 
-application = ProtocolTypeRouter({})
+from trips.consumers import TaxiConsumer
+from .middleware import TokenAuthMiddlewareStack
+
+application = ProtocolTypeRouter({
+  'websocket': TokenAuthMiddlewareStack( 
+    URLRouter([
+        path('taxi/', TaxiConsumer),
+    ])
+    ),
+})
--- server/taxi/trips/models.py
+++ server/taxi/trips/models.py
@@ -3,33 +3,49 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.shortcuts import reverse
+from django.conf import settings
 
 # Create your models here.
 class User(AbstractUser):
   pass
 
 class Trip(models.Model):
-  REQUESTED = 'REQUESTED'
-  STARTED = 'STARTED'
-  IN_PROGRESS = 'IN_PROGRESS'
-  COMPLETED = 'COMPLETED'
-  STATUSES = (
+    REQUESTED = 'REQUESTED'
+    STARTED = 'STARTED'
+    IN_PROGRESS = 'IN_PROGRESS'
+    COMPLETED = 'COMPLETED'
+    STATUSES = (
       (REQUESTED, REQUESTED),
       (STARTED, STARTED),
       (IN_PROGRESS, IN_PROGRESS),
       (COMPLETED, COMPLETED),
-  )
+    )
 
-  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-  created = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_now=True)
-  pick_up_address = models.CharField(max_length=255)
-  drop_off_address = models.CharField(max_length=255)
-  status = models.CharField(
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    pick_up_address = models.CharField(max_length=255)
+    drop_off_address = models.CharField(max_length=255)
+    status = models.CharField(
       max_length=20, choices=STATUSES, default=REQUESTED)
+    driver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.DO_NOTHING,
+        related_name='trips_as_driver'
+    )
+    rider = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    null=True,
+    blank=True,
+    on_delete=models.DO_NOTHING,
+    related_name='trips_as_rider'
+    )
 
-  def __str__(self):
+
+    def __str__(self):
       return f'{self.id}'
 
-  def get_absolute_url(self):
+    def get_absolute_url(self):
       return reverse('trip:trip_detail', kwargs={'trip_id': self.id})
